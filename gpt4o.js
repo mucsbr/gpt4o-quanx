2d2c4eab22e2b545c597a26991cfb298d24c177d
beingS <mucsbr@sina.cn>
2024-05-14 17:47:57 +0800

    Update gpt4o.js

--- gpt4o.js
+++ gpt4o.js
@@ -1,10 +1,10 @@
 /*
 
-for chenjian??
+for gpt4o
 
 [rewrite_local]
 
-^^https://ab.chatgpt.com/v1/ url script-response-body https://raw.githubusercontent.com/510004015/Quantumult_X/Remote/Premium/WPSOffice.js
+^https://ab.chatgpt.com/v1/ url script-response-body https://raw.githubusercontent.com/mucsbr/gpt4o-quanx/main/gpt4o.js
 
 [mitm]
 
@@ -15,13 +15,10 @@ hostname = ab.chatgpt.com
 function setAllFalseValuesToTrue(obj) {
     for (let key in obj) {
         if (typeof obj[key] === 'object' && obj[key] !== null) {
-            // 如果是对象，就递归处理
             setAllFalseValuesToTrue(obj[key]);
         } else if (key === 'value' && obj[key] === false) {
-            // 找到了匹配的key-value对，修改它
             obj[key] = true;
         } else if (key === 'gateValue' && obj[key] === 'false') {
-            // 找到了匹配的key-value对，修改它
             obj[key] = 'true';
         }
     }
